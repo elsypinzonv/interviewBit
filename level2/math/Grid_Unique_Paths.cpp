235f762d275107a186a2f91a1dc2fb5bdb965419
Elsy Pinzon <elsy.pinzon@wizeline.com>
2018-04-27 11:35:43 -0500

    add grid unique paths

--- level2/math/Grid_Unique_Paths.cpp
+++ level2/math/Grid_Unique_Paths.cpp
@@ -3,3 +3,12 @@ Grid Unique Paths
 https://www.interviewbit.com/problems/grid-unique-paths/
 */
 
+int dp(int i, int j, int A, int B){
+    if(i >= A || j >= B) return 0;
+    if(i == A -1 || j == B-1)
+    return dp(i +1, j, A, B) + dp(i, j+1, A, B);
+}
+
+int Solution::uniquePaths(int A, int B) {
+    return dp(0,0, A,B);
+}
